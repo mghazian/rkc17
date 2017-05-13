b3e75a2acd082368899707448c60386d1773c6ee
mathovonic96 <mathovonic96@gmail.com>
2017-05-13 11:26:57 +0700

    Update Admin.php

--- src/application/controllers/Admin.php
+++ src/application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Controller {
+class Admin extends CI_Controller {
 	function __construct()
 	{	
 		parent::__construct();
@@ -10,13 +10,35 @@ class User extends CI_Controller {
 	function index()
 	{
 		
-		//$this->load->view('header');
-		//$this->load->view('navbar');
-
-		$this->load->view('isi');
-
-		//$this->load->view('footer');
-		
-		//$this->load->view ('test');
+		$this->load->view('header');
+		$this->load->view('Admin/navbar');
+		//$this->load->view('isi');
+		$this->load->view('Admin/left_sidebar');
+		$this->load->view('Admin/dashboard');
+		$this->load->view('footer');
+	}
+	function profil()
+	{
+		$this->load->view('header');
+		$this->load->view('Admin/navbar');
+		$this->load->view('Admin/left_sidebar');
+		$this->load->view('Admin/profil');
+		$this->load->view('footer');
+	}
+	function pencarian()
+	{
+		$this->load->view('header');
+		$this->load->view('Admin/navbar');
+		$this->load->view('Admin/left_sidebar');
+		$this->load->view('Admin/pencarian');
+		$this->load->view('footer');
+	}
+	function pengaturan()
+	{
+		$this->load->view('header');
+		$this->load->view('Admin/navbar');
+		$this->load->view('Admin/left_sidebar');
+		$this->load->view('Admin/pengaturan');
+		$this->load->view('footer');
 	}
 }
